b818461bd88d8238511cdf34f3efbaaf15642f19
prashant1307 <prashantlorgev8@gmail.com>
2022-09-29 00:15:48 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from "./components/Footer/Footer"
 import Homepage from './pages/Homepage';
+import { Timer } from './pages/Timer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Homepage/>
       <Footer/>
+      <Timer/>
     </div>
   );
 }
